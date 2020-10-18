ac2f82cf14b3bf152cb061b2303573d1161754df
Alex Brasil <59840679+abrasil321@users.noreply.github.com>
2020-10-17 23:19:01 -0600

    message

--- web/dispList.js
+++ web/dispList.js
@@ -1,8 +1,10 @@
 
 function hiding() {
     var x = document.getElementById("list");
+    
     if (x.style.display === "block") {
       x.style.display = "none";
+      document.getElementById('bt').innerHTML = "Hide All Users";
     } else {
       x.style.display = "block";
     }
