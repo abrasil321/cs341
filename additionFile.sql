b07d0b739868c8d4e4b99dabd59b244230b4a979
Alex Brasil <59840679+abrasil321@users.noreply.github.com>
2020-10-17 22:58:42 -0600

    message

--- additionFile.sql
+++ additionFile.sql
@@ -1,15 +1,17 @@
-INSERT INTO plan (goal, descrip)
-    VALUES ('build muscle', 'low Carb diet with high protein');
-
-INSERT INTO plan (goal, descrip)
-    VALUES ('gain Weight', 'medium carbo diet, high protein and cardio');
-
-INSERT INTO plan (goal, descrip)
-    VALUES ('mantain Weight', 'Cicle of carbs with low/zero carb, high protein and high fat');
-
-
-SELECT goal, descrip
-    from plan;
+INSERT INTO theUser (user_id, firstName, lastName, emailAddress)
+    VALUES (4, 'Peter', 'Robson', 'bra17001@byui.edu');
+INSERT INTO theUser (user_id, firstName, lastName, emailAddress)
+    VALUES (5, 'Mark', 'Stevan', 'bra17001@byui.edu');
+INSERT INTO theUser (user_id, firstName, lastName, emailAddress)
+    VALUES (6, 'Luke', 'Hart', 'bra17001@byui.edu');
+INSERT INTO theUser (user_id, firstName, lastName, emailAddress)
+    VALUES (7, 'Steven', 'Davis', 'bra17001@byui.edu');
+INSERT INTO theUser (user_id, firstName, lastName, emailAddress)
+    VALUES (8, 'Mary', 'Benson', 'bra17001@byui.edu');
+INSERT INTO theUser (user_id, firstName, lastName, emailAddress)
+    VALUES (9, 'Julie', 'Young', 'bra17001@byui.edu');
+INSERT INTO theUser (user_id, firstName, lastName, emailAddress)
+    VALUES (10, 'Emma', 'Correa', 'bra17001@byui.edu');
 
 SELECT user_id, firstName, lastName, emailAddress
     from theUser;
