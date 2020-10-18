aacb8db5525193d810e4a0557bbbbdb8bc47acad
Alex Brasil <59840679+abrasil321@users.noreply.github.com>
2020-10-17 18:24:57 -0600

    message

--- web/index.php
+++ web/index.php
@@ -28,18 +28,18 @@
             {
                 $statement = $db->prepare('Select * FROM theuser');
                 $statement->execute();
+                echo "<table>
+                        <tr>
+                            <th>User ID</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                        </tr>";
                 while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
                     $user_id = $row['user_id'];
                     $first_name = $row['firstname'];
                     $last_name = $row['lastname'];
 
-                    echo "<table>
-                        <tr>
-                            <th>User ID</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                        </tr>
-                        <tr>
+                    echo "<tr>
                             <td> $user_id</td>
                             <td> $first_name</td>
                             <td> $last_name</td>
