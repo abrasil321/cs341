ed525586ff8ffb226101544c8a15197b0251a026
Alex Brasil <59840679+abrasil321@users.noreply.github.com>
2020-10-17 22:16:40 -0600

    message

--- web/dispList.js
+++ web/dispList.js
@@ -3,7 +3,7 @@ function searchById(){
     // let user_id = document.getElementById("id");
     // for (let i = 0; i < $_SESSION["user_id"].length; i++) {
     //     if ($_SESSION["user_id"] == user_id){
-            document.getElementById("dispID").innerHTML =  'Session . $_SESSION["user_id"] . <br>;';
+            document.getElementById("dispID").innerHTML =  '<?php echo Session . $_SESSION["user_id"] . <br>; ?>';
             // echo "Session " . $_SESSION["first_name"] . "<br>";
             // echo "Session " . $_SESSION["last_name"] . "<br>";
         // }
