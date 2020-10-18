15eb3159988d6555a921ffdbcd7d597a76fbb7f4
Alex Brasil <59840679+abrasil321@users.noreply.github.com>
2020-10-17 19:33:36 -0600

    message

--- web/index.php
+++ web/index.php
@@ -25,7 +25,35 @@
     <button type="button" onclick="">Search</button>
     <button type="button" onclick="list()">Display List</button>
     <div>
-        <p id="list"></p>
+    <?php
+                            try 
+                            {
+                                $statement = $db->prepare('Select * FROM theuser');
+                                $statement->execute();
+                                echo "<table>
+                                        <tr>
+                                            <th>User ID</th>
+                                            <th>First Name</th>
+                                            <th>Last Name</th>
+                                        </tr>";
+                                while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+                                    $user_id = $row['user_id'];
+                                    $first_name = $row['firstname'];
+                                    $last_name = $row['lastname'];
+
+                                    echo "<tr>
+                                            <td> $user_id</td>
+                                            <td> $first_name</td>
+                                            <td> $last_name</td>
+                                        </tr>";
+                                    
+                                }
+                            } 
+                            catch (Exception $ex) 
+                            {
+                                echo "$ex";
+                            }
+                        ?>
     </div>
 </body>
 </html>
