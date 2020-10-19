27a8c45e68786cec7d72ca6fcb07140cfe1bae49
Alex Brasil <59840679+abrasil321@users.noreply.github.com>
2020-10-18 19:04:23 -0600

    added

--- web/index.php
+++ web/index.php
@@ -32,42 +32,42 @@ session_start();
             require "list.php";
         ?>                    
     </div>
-    <div id="search">
+    <!-- <div id="search">
         <?php
-            try 
-            {
-                $statement = $db->prepare('Select * FROM theuser');
-                $statement->execute();
+            // try 
+            // {
+            //     $statement = $db->prepare('Select * FROM theuser');
+            //     $statement->execute();
     
-                while ($row = $statement->fetch(PDO::FETCH_ASSOC)) 
-                {
-                    $user_id = $row['user_id'];
-                    $first_name = $row['firstname'];
-                    $last_name = $row['lastname'];
+            //     while ($row = $statement->fetch(PDO::FETCH_ASSOC)) 
+            //     {
+            //         $user_id = $row['user_id'];
+            //         $first_name = $row['firstname'];
+            //         $last_name = $row['lastname'];
 
-                    $_SESSION["user_id"] = $user_id;
-                    $_SESSION["first_name"] = $first_name;
-                    $_SESSION["last_name"] = $last_name;
+            //         $_SESSION["user_id"] = $user_id;
+            //         $_SESSION["first_name"] = $first_name;
+            //         $_SESSION["last_name"] = $last_name;
                     
-                    $ids = array();
-                    array_push($ids, $_SESSION["user_id"]);
-                    print_r($ids);
+            //         $ids = array();
+            //         array_push($ids, $_SESSION["user_id"]);
+            //         print_r($ids);
 
-                    $fnames = array();
-                    array_push($ids, $_SESSION["first_name"]);
-                    print_r($fnames);
+            //         $fnames = array();
+            //         array_push($ids, $_SESSION["first_name"]);
+            //         print_r($fnames);
 
-                    $lnames = array();
-                    array_push($ids, $_SESSION["last_name"]);
-                    print_r($lnames);
-                }
-            } 
-            catch (Exception $ex) 
-            {
-                echo "$ex";
-            }
-        ?>        
-    </div>
+            //         $lnames = array();
+            //         array_push($ids, $_SESSION["last_name"]);
+            //         print_r($lnames);
+            //     }
+            // } 
+            // catch (Exception $ex) 
+        //     {
+        //         echo "$ex";
+        //     }
+        // ?>        
+    </div> -->
 
 </body>
 </html>
