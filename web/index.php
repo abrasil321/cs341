904e6ca17de2e792bd2badb15c4b92c237727a94
Alex Brasil <59840679+abrasil321@users.noreply.github.com>
2020-10-18 18:25:32 -0600

    added

--- web/index.php
+++ web/index.php
@@ -29,42 +29,19 @@ session_start();
     <button id="bt" type="button" onclick="hiding()">Display All Users</button>
     <div id="list" hidden>
     <?php
-                            try 
-                            {
-                                $statement = $db->prepare('Select * FROM theuser');
-                                $statement->execute();
-                                echo "<table>
-                                        <tr>
-                                            <th>User ID</th>
-                                            <th>First Name</th>
-                                            <th>Last Name</th>
-                                        </tr>";
-                                while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-                                    $user_id = $row['user_id'];
-                                    $first_name = $row['firstname'];
-                                    $last_name = $row['lastname'];
+        require "list.php";
 
-                                    echo "<tr>
-                                            <td> $user_id</td>
-                                            <td> $first_name</td>
-                                            <td> $last_name</td>
-                                        </tr>";
 
-                                    // $_SESSION["user_id"] = $user_id;
-                                    // $_SESSION["first_name"] = $first_name;
-                                    // $_SESSION["last_name"] = $last_name;
+        // $_SESSION["user_id"] = $user_id;
+        // $_SESSION["first_name"] = $first_name;
+        // $_SESSION["last_name"] = $last_name;
 
-                                    // echo  $_SESSION["user_id"] . "<br>";
-                                    // echo "Session " . $_SESSION["first_name"] . "<br>";
-                                    // echo "Session " . $_SESSION["last_name"] . "<br>";
-                                    
-                                }
-                            } 
-                            catch (Exception $ex) 
-                            {
-                                echo "$ex";
-                            }
-                        ?>
+        // echo  $_SESSION["user_id"] . "<br>";
+        // echo "Session " . $_SESSION["first_name"] . "<br>";
+        // echo "Session " . $_SESSION["last_name"] . "<br>";                        
+
+    ?>
+                           
     </div>
 
 </body>
