0b41a2c3c0dd7fa9a13ce2ed049b51a7fd13afd6
Alex Brasil <59840679+abrasil321@users.noreply.github.com>
2020-10-24 10:32:38 -0600

    added

--- web/newUser.php
+++ web/newUser.php
@@ -23,10 +23,11 @@
         <div id="two">Enter First Name: <input type="text" name="fname" required></div><br>
         <div id="two">Enter Last Name: <input type="text" name="lname" require></div><br>
         <div id="two">Enter Email Address: <input type="email" name="emaill" require></div><br>
-        <div id="two">Choose your goal: <input type="radio" name="radio" id="Cutting" onchange="checkBox()"> Cutting
+        <div id="two">Choose your goal: <input type="radio" name="radio" id="Cutting"> Cutting
                           <input type="radio" name="radio" id="Maintaining"> Maintaining
                           <input type="radio" name="radio" id="Bulking"> Bulking</div><br>
                           <br>
+        <div id="two">Description: <textarea name="textArea" name="textArea" placeholder="Describe your personal goal"></textarea>
         <button type="submit">Submit</button>
         <br>
     </form>
