9f6a1480b0cafb7e27332ee25dc027db0fe590f9
Alex Brasil <59840679+abrasil321@users.noreply.github.com>
2020-10-23 19:33:06 -0600

    added

--- web/newUser.php
+++ web/newUser.php
@@ -15,11 +15,13 @@
     <div id="head">
         Making your dream body become true
     </div>
+    <br>
+    <br>
     <form method="$_POST" action="newUser_confirm.php">
         <br>
-        <div id="one">Enter First Name: <input type="text" name="fname" required></div><br>
+        <div id="two">Enter First Name: <input type="text" name="fname" required></div><br>
         <div id="two">Enter Last Name: <input type="text" name="lname" require></div><br>
-        <div id="one">Enter Email Address: <input type="email" name="emaill" require></div><br>
+        <div id="two">Enter Email Address: <input type="email" name="emaill" require></div><br>
         <div id="two">Choose your goal: <input type="radio" name="radio" id="Cutting" onchange="checkBox()">Cutting
                           <input type="radio" name="radio" id="Maintaining"> Maintaining
                           <input type="radio" name="radio" id="Bulking"> Bulking</div><br>
